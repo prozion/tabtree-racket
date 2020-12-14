34597aae13b8536d31cc5f48a41438fa5c8192b2
Denis Shirshov <denis.shirshov@gmail.com>
2020-12-14 17:01:24 +0300

    1

---
+++ html.rkt
@@ -0,0 +1,209 @@
+#lang racket
+
+(require odysseus)
+(require "main.rkt")
+(require "utils.rkt")
+
+(provide (all-defined-out))
+
+(define-namespace-anchor anchor)
+(define ns-own (namespace-anchor->namespace anchor))
+
+(define <tabtree> (make-parameter (hash)))
+(define <iteratees> (make-parameter (hash)))
+
+(define (get-ht-item-name item)
+  (titlefy (or ($ name item) ($ id item) "")))
+
+; (define (url-if-possible name url)
+;   (if (and url (non-empty-string? url))
+;     (format "<a href=\"~a\" target=\"_blank\">~a</a>" (httpify url) (namefy name))
+;     (namefy name)))
+
+(define (namefy-with-url id name . urls)
+  (let* (
+        (urls (filter-not nil? urls))
+        (url (and (not-empty? urls) (car urls)))
+        (title (if (non-empty-string? name) name (namefy id))))
+    (if (and url (non-empty-string? url))
+      (format "<a href=\"~a\" target=\"_blank\">~a</a>" (httpify url) title)
+      title)))
+
+(define (extend-txt prefix (suffix ""))
+  (λ (txt)
+      (if (and txt (non-empty-string? txt))
+        (str prefix txt suffix)
+        "")))
+
+(define-catch (fullpath-to-data tabtree-root tabtree-path)
+  (let* (
+        (tabtree-path (split (->string tabtree-path) "."))
+        (tabtree-file (first tabtree-path))
+        (tab-tree-filepath (string->path (str tabtree-root "/" tabtree-file ".tree")))
+        (tab-mtree-filepath (string->path (str tabtree-root "/" tabtree-file ".mtree")))
+        (mtree? (file-exists? tab-mtree-filepath))
+        (tree? (file-exists? tab-tree-filepath))
+        (_ (when (and (not mtree?) (not tree?)) (error (format "process-html-template: no corresponded tabtree file for the path: ~a" tabtree-path))))
+        (tabtree (if mtree? (parse-tab-mtree tab-mtree-filepath) (parse-tab-tree tab-tree-filepath)))
+        (datapath (cdr tabtree-path))
+        (data (if (empty? datapath) tabtree (or (get-$1 datapath tabtree) (get-$2 datapath tabtree))))
+        )
+    data))
+
+(define-catch (fill-template data html-template #:ns (ns ns-own))
+  (cond
+    ((hash-empty? data) "")
+    (else
+      (let* (
+            ;; replace keyword <this> with a hash object (data)
+            (html-template (string-replace html-template "<this>" (~s data)))
+            (snippets (map
+                        (λ (x) (second x))
+                        (get-matches #rx"{(.*?)}" html-template)))
+            (result (for/fold
+                      ((res html-template))
+                      ((snippet snippets))
+                      (let* (
+                            (snippet-lst (split snippet ":"))
+                            (snippet-varnames (split (first snippet-lst) ","))
+                            (snippet-lambda (if (> (length snippet-lst) 1) (cdr snippet-lst) #f))
+                            (snippet-lambda (cond
+                                              ((not snippet-lambda) snippet-lambda)
+                                              ((equal? "" snippet-lambda) #f)
+                                              ; if some ':'s in lambda itself - merge on : again
+                                              ((> (length snippet-lambda) 1) (implode snippet-lambda ":"))
+                                              (else (car snippet-lambda))))
+                            (snippet-lambda (if snippet-lambda
+                                              ; (eval (read (open-input-string (str "(λ args (apply " snippet-lambda " args))"))) ns-own)
+                                              (eval (read (open-input-string snippet-lambda)) ns)
+                                              (λ args (implode args ",")))) ; leave e.g. "a,b,c" as it was
+                            (prefix-string (if (> (length snippet-lst) 2) (third snippet-lst) ""))
+                            (postfix-string (if (> (length snippet-lst) 3) (fourth snippet-lst) ""))
+                            (result-core (apply
+                                            snippet-lambda
+                                            (map
+                                              (λ (x) (cond
+                                                      ((re-matches? "\"\"" x) #f)
+                                                      ((re-matches? "\".*?\"" x) (triml (trimr x)))
+                                                      ((re-matches? "[0-9]+" x) (->number x))
+                                                      (else (hash-ref* data x #f))))
+                                              snippet-varnames)))
+                            (result (if (and result-core (non-empty-string? result-core))
+                                      result-core
+                                      ""))
+                            )
+                        (string-replace
+                          res
+                          (str "{" snippet "}")
+                          result
+                          )))))
+        result))))
+
+(define-catch (snippet->data snippet #:ns namespace #:tabtree-root (tabtree-root "."))
+  (let* (
+        (snippet-parts (string-split snippet ":"))
+        (collection-part (first snippet-parts))
+        (collections (string-split collection-part ","))
+        (collection-paths (map (λ (x) (let* ((collection-part-parts (string-split x "//"))
+                                            (path-part (if (> (length collection-part-parts) 1) (second collection-part-parts) (first collection-part-parts))))
+                                          path-part))
+                                collections))
+        (tabtrees (for/fold
+                    ((res (list)))
+                    ((collection collections))
+                    (let* ((collection-lst (string-split collection "//"))
+                          (tabtree-root (if (= (length collection-lst) 1) tabtree-root (first collection-lst)))
+                          (tabtree-path (if (= (length collection-lst) 1) (first collection-lst) (second collection-lst)))
+                          (tabtree-path (split tabtree-path "."))
+                          (tabtree (fullpath-to-data tabtree-root (first tabtree-path))))
+                      (pushr res tabtree))))
+        (function-part (cond
+                          ((> (length snippet-parts) 2)
+                              (eval (read (open-input-string (implode (cdr snippet-parts) ":"))) namespace))
+                          ((= (length snippet-parts) 2)
+                              (eval (read (open-input-string (second snippet-parts))) namespace))
+                          (else identity)))
+        (parameter-values (map (λ (collection-path tabtree) (get-$1 (rest (split collection-path ".")) tabtree)) collection-paths tabtrees))
+        (result (apply function-part parameter-values)))
+    result))
+
+(define-catch (process-html-template filepath #:tabtree-root (tabtree-root "") #:namespace (namespace #f))
+  (define-catch (get-cycle-object lst)
+    (let* (
+          (template-html (first lst))
+          (collection-part (split (second lst) ":"))
+          (collection (first collection-part))
+          (collection-lst (string-split collection "//"))
+          (tabtree-root (if (= (length collection-lst) 1) tabtree-root (first collection-lst)))
+          (tabtree-path (if (= (length collection-lst) 1) (first collection-lst) (second collection-lst)))
+          (tabtree-path (split tabtree-path "."))
+          (html-part (third lst))
+          (tabtree (fullpath-to-data tabtree-root (first tabtree-path)))
+          (_ (<tabtree> tabtree))
+          (_ (<iteratees> (get-leaves tabtree)))
+          (snippet-lambda (if (> (length collection-part) 1) (cdr collection-part) #f))
+          (snippet-lambda (cond
+                            ((not snippet-lambda) snippet-lambda)
+                            ((equal? "" snippet-lambda) #f)
+                            ((> (length snippet-lambda) 1) (implode snippet-lambda ":"))
+                            (else (car snippet-lambda))))
+          (snippet-lambda (if snippet-lambda
+                            (eval (read (open-input-string snippet-lambda)) namespace)
+                            identity))
+          (cycled-data (get-$3 (cdr tabtree-path) tabtree))
+          ; do filtering, mapping or other operations over collection of data
+          (cycled-data (snippet-lambda cycled-data))
+          (result (hash 'matched-template template-html 'collection collection 'data cycled-data 'html html-part))
+          )
+      result))
+  (define-catch (execute-code-snippet code-snippet)
+    (eval (read (open-input-string (str "(" code-snippet ")"))) namespace))
+  (let* ((filepath (if (path? filepath) filepath (string->path filepath)))
+        (html-res (read-file filepath))
+        (code-snippets-before (get-matches #rx"%%\\((.+?)\\)%%" html-res))
+        (code-snippets-before (map second code-snippets-before))
+        (code-snippets-before-results (map execute-code-snippet code-snippets-before))
+        (html-res (for/fold
+                    ((res html-res))
+                    ((code-snippet-r code-snippets-before-results) (code-snippet code-snippets-before))
+                    (string-replace res (str "%%(" code-snippet ")%%") (if (and code-snippet-r (not (void? code-snippet-r))) (->string code-snippet-r) ""))))
+        (cycle-snippets (get-matches #rx"\\[{(.+?)}:(.+?)\\]" html-res))
+        (cycle-objects (map get-cycle-object cycle-snippets))
+        (html-res (for/fold
+                    ((res html-res))
+                    ((cycle-object cycle-objects))
+                    (string-replace
+                      res
+                      ($ matched-template cycle-object)
+                      (for/fold
+                        ((res2 ""))
+                        ((data-item (or ($ data cycle-object) (list (hash)))))
+                        (str
+                          res2
+                          (fill-template data-item ($ html cycle-object) #:ns namespace))))))
+        (snippets (map
+                    (λ (x) (second x))
+                    (get-matches #rx"{{(.*?)}}" html-res)))
+        (html-res (for/fold
+                    ((res html-res))
+                    ((snippet snippets))
+                    (string-replace res (str "{{" snippet "}}") (snippet->data snippet #:ns namespace #:tabtree-root tabtree-root))))
+        (code-snippets (get-matches #rx"%\\((.+?)\\)%" html-res))
+        (code-snippets (map second code-snippets))
+        (code-snippets-results (map execute-code-snippet code-snippets))
+        (html-res (for/fold
+                    ((res html-res))
+                    ((code-snippet-r code-snippets-results) (code-snippet code-snippets))
+                    (string-replace
+                      res
+                      (str "%(" code-snippet ")%")
+                      (if (and code-snippet-r (not (void? code-snippet-r)))
+                        (->string code-snippet-r)
+                        ""))))
+        ; clean empty lines after snippets
+        (html-res (string-replace html-res #px"\n\\s*?\r?\n" "\n\n"))
+        (html-res (string-replace html-res #px"\n\\s+?\n" "\n\n"))
+        (html-res (string-replace html-res #px"(?m:\n\n+)" "\n\n"))
+        )
+    ; (--- html-res)
+    html-res))
---
+++ utils.rkt
@@ -0,0 +1,192 @@
+#lang racket
+
+;; in this file we represent functions that modify elements of the tree
+
+(require odysseus)
+(require "main.rkt")
+(require compatibility/defmacro)
+
+(provide (all-defined-out))
+
+(define (make-index items (index-name "i"))
+  (for/fold
+    ((res empty))
+    ((item items) (idx (in-naturals 1)))
+    (pushr
+      res
+      (hash-union
+        (hash index-name idx)
+        item))))
+
+; {w,w<r,w>r:first-existed}
+(define (first-existed (fallback-value #f))
+  (λ args
+    (let ((existed (filter-not nil? args)))
+      (if (empty? existed)
+        fallback-value
+        (first existed)))))
+
+(define (add-type val list-of-hashes #:attr (attr 'type))
+  (map
+    (λ (x) (hash-union (hash attr val) x))
+    list-of-hashes))
+
+(define-catch (tabtree-item->string item keys-order #:remove-derived-keys (remove-derived-keys #t))
+  (define (parameter->string key params (delimeter ","))
+    (cond
+      ((list? params) (implode params delimeter))
+      ((re-matches? "-f$" (->string key)) (format "`~a`" params)) ; a code snippet
+      ((and (string? params) (re-matches? " " params)) (format "\"~a\"" params))
+      (else (->string params))))
+  (let* ((id ($ id item))
+        (keys-to-print (filter-not (λ (x) (let ((x (->string x))) (or (string-prefix? x "_") (equal? x "id")))) (hash-keys item)))
+        (keys-to-print (if remove-derived-keys
+                            (filter-not (λ (k) (regexp-match? #rx"^\\+.+" (->string k))) keys-to-print)
+                            keys-to-print))
+        (keys-ordered (if keys-order
+                            (for/fold
+                              ((res empty))
+                              ((ko keys-order))
+                              (if (indexof*? keys-to-print ko)
+                                (pushr res (->symbol ko))
+                                res))
+                            empty))
+        (tail-keys (minus keys-to-print keys-ordered)))
+    (for/fold
+      ((res (format "~a" id)))
+      ((key (append (or keys-ordered empty) (or tail-keys empty))))
+      (format "~a ~a:~a" res key (parameter->string key (hash-ref item key))))))
+
+(define-catch (tabtree->string tabtree #:parent (parent-item #f) #:level (level 0) #:keys-order (keys-order #f) #:sort-f (sort-f (λ (a b) (< (->number (or ($ _order a) 0)) (->number (or ($ _order b) 0))))))
+  (let* ((sorted-keys (sort (hash-keys tabtree) sort-f))
+        (keys-order (or (and parent-item ($ keys-order parent-item)) keys-order)) ; such a way we can define keys-order only in the topmost element and it will be inherited by all lower elements unless they define their own keys-order
+        (keys-order (cond
+                      ((string? keys-order) (string-split keys-order ","))
+                      ((list? keys-order) keys-order)
+                      (else keys-order))))
+    (for/fold
+      ((res ""))
+      ((k sorted-keys))
+      (let* ((leaf? (hash-empty? (hash-ref tabtree k))))
+        (cond
+          (leaf?
+            (format "~a~a~a"
+                    (if (or (not-empty-string? res) parent-item) (format "~a~n" res) res)
+                    (dupstr "\t" level)
+                    (tabtree-item->string k (or ($ keys-order k) keys-order))))
+          (else
+            (format "~a~a~a~a"
+                    (if (or (not-empty-string? res) parent-item) (format "~a~n" res) res) ; don't skip the first line in the file
+                    (dupstr "\t" level)
+                    (tabtree-item->string k (or ($ keys-order k) keys-order)) ; first check if current item has keys-order attribute - then apply this one, otherwise use normal keys-order from the parent
+                    (tabtree->string (hash-ref tabtree k) #:parent k #:level (+ 1 level) #:keys-order keys-order #:sort-f sort-f))))))))
+
+; hashtree -> list-of hash
+(define (get-first-level hashtree)
+  (hash-keys hashtree))
+
+; hashtree -> hashtree
+(define (get-level-below hashtree)
+  (let* ((root-element (get-root-item hashtree)))
+    (hash-ref hashtree root-element #f)))
+
+(define (default-sorter a b)
+  (a-z ($ id a) ($ id b)))
+
+(define default-sort-f A-Za-z)
+(define default-sort-by 'id)
+
+(define (hash-id h)
+  ($ id h))
+
+; Important! Tabtree file should contain only one root element. Other first level elements and their chikdren will be deleted after sorting.
+(define-catch (tabtree-sort-and-print #:tabtree (tabtree #f) #:tabtree-file (tabtree-file #f) #:ns (ns #f) #:new-treefile (new-treefile #f) #:sort-by (sort-by default-sort-by) #:sort-by-f (sort-by-f #f) #:sort-f (sort-f default-sort-f))
+  (define-catch (tabtree-sort-rec root-item root-hashtree #:sort-by sort-by #:sort-f sort-f #:sort-by-f sort-by-f)
+    (cond
+      ((not (hash? root-hashtree)) root-hashtree)
+      ((hash-empty? root-hashtree) root-hashtree) ; empty list
+      ((not-hashes? (hash-keys root-hashtree)) root-hashtree) ; end element
+      (else
+        (let* (
+              (embedded-sort-by (and ($ sort-by root-item) ns (read (open-input-string ($ sort-by root-item) ns))))
+              (embedded-sort-by (and embedded-sort-by (->symbol embedded-sort-by)))
+              (embedded-sort-f (and ($ sort-f root-item) ns (read (open-input-string ($ sort-f root-item) ns))))
+              (embedded-sort-f (and embedded-sort-f (->symbol embedded-sort-f)))
+              (sort-by (or embedded-sort-by sort-by))
+              (sort-f (or embedded-sort-f sort-f))
+              (root-hashtree-keys (hash-keys root-hashtree))
+              (sorter (λ (a b) (let ((a-val (if sort-by-f
+                                                (sort-by-f a)
+                                                (hash-ref a sort-by #f)))
+                                    (b-val (if sort-by-f
+                                              (sort-by-f b)
+                                              (hash-ref b sort-by #f))))
+                                  (cond
+                                    ((and a-val b-val)
+                                      ((eval sort-f ns) a-val b-val))
+                                    (else
+                                      (let ((a-val (hash-ref a default-sort-by #f))
+                                            (b-val (hash-ref b default-sort-by #f)))
+                                      (default-sort-f a-val b-val))))))))
+            (for/hash
+              ((k (sort root-hashtree-keys sorter)) (i (range 1 (inc (length root-hashtree-keys)))))
+              (values (hash-union (hash '_order i) k) (tabtree-sort-rec k (hash-ref root-hashtree k) #:sort-by sort-by #:sort-by-f sort-by-f #:sort-f sort-f)))))))
+  (let* ((hashtree
+                (cond
+                  (tabtree tabtree)
+                  (tabtree-file (parse-tab-tree tabtree-file))
+                  (else (errorf "neither #tabtree nor #tabtree-file are given in tabtree-sort-and-print"))))
+        (root-item (get-root-item hashtree))
+        (hashtree-sorted (hash
+                            root-item
+                            (tabtree-sort-rec root-item (get-level-below hashtree) #:sort-by sort-by #:sort-by-f sort-by-f #:sort-f sort-f)))
+        (new-treefile-name (if new-treefile
+                              new-treefile
+                              (cond
+                                (tabtree-file
+                                  (let*
+                                      ((filename-parts (string-split tabtree-file #rx"(?<=[A-Za-zА-Яа-я0-9_])\\.(?=[A-Za-zА-Яа-я0-9_])"))
+                                      (aname (car filename-parts))
+                                      (ext (and (not-empty? (cdr filename-parts)) (cadr filename-parts))))
+                                    (str aname "_" (if ext (str "." ext) ""))))
+                                (else "_new_tabtree.tree"))))
+        (new-treefile-str (tabtree->string hashtree-sorted)))
+      (write-file new-treefile-name new-treefile-str)
+      #t))
+
+(define-catch (filter-tabtree f tabtree)
+  (cond
+    ((hash-empty? tabtree) tabtree)
+    (else
+      (let* ((filtered-keys (filter f (hash-keys tabtree))))
+        (for/fold
+          ((res (hash)))
+          ((k filtered-keys))
+          (hash-union res (hash k (filter-tabtree f (hash-ref tabtree k)))))))))
+
+(define (set-current-page-mark id curpage-id (class "current_page_nav"))
+  (if (equal? (->string id) (->string curpage-id))
+    (format "class=\"~a\"" class)
+    ""))
+
+(define (tabtree-true? v)
+  (cond
+    ((equal? (->string v) "<t>") #t)
+    ((equal? (->string v) "<f>") #f)
+    (else #t)))
+
+; get either '<key>' or inherited '<key>+' value of item
+(define-macro ($* k item)
+  (let ((kplus (string-append "+" (format "~a" k))))
+    `(or
+        (hash-ref* ,item ',k)
+        (hash-ref* ,item ,kplus))))
+
+(define-catch (without-special-keys item)
+  (let ((special-keys-re "^[+_].+|id|.+?-id$"))
+    (for/fold
+      ((res (hash)))
+      ((k (hash-keys item)))
+      (cond
+        ((re-matches? special-keys-re (->string k)) res)
+        (else (hash-insert res (cons k (hash-ref item k))))))))
