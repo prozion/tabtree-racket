dd4e15b9e81b0461f5ff3e96040cb6e26c058516
Denis Shirshov <denis.shirshov@gmail.com>
2021-04-17 11:17:45 +0300

    4.3

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "tabtree")
-(define deps '("odysseus"))
+(define deps '("odysseus" "compatibility-lib"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("docs/tabtree.scrbl")))
 (define pkg-desc "A library to handle files of Tabtree format")
