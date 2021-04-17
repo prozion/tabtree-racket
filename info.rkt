6df7271b010d502f925696bf93f3071b655099f9
Denis Shirshov <denis.shirshov@gmail.com>
2021-04-17 11:10:20 +0300

    4.2

--- info.rkt
+++ info.rkt
@@ -4,5 +4,4 @@
 (define deps '("odysseus"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("docs/tabtree.scrbl")))
-(define pkg-desc "Library with handy general-purpose functions and macro as well as specific ones. Uses as a midlayer between a bit nurdy vanilla Racket and practical problems met in the everyday life out on the street")
-(define version "2021.4")
+(define pkg-desc "A library to handle files of Tabtree format")
