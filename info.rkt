b8884fe7a85799970ed9660990f34c5af2555391
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-05-26 13:37:40 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
 
 (define collection "tabtree")
-(define deps '("odysseus" "compatibility-lib"))
-(define build-deps '("racket-doc" "scribble-lib"))
+(define deps '("base"
+               "odysseus" "compatibility-lib"))
+(define build-deps '("rackunit-lib"
+                     "racket-doc" "scribble-lib"))
 (define scribblings '(("docs/tabtree.scrbl")))
 (define pkg-desc "A library to handle files of Tabtree format")
